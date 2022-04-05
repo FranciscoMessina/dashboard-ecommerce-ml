fc14237893f68eafb4f9acfb40465c7779721f32
FranciscoMessina <francisco@messina.ws>
2022-04-05 17:36:00 -0300

    Changed text orden in question component

--- src/components/question/InputWithAutocomplete.tsx
+++ src/components/question/InputWithAutocomplete.tsx
@@ -1,8 +1,8 @@
 import { Combobox } from '@headlessui/react'
-import { Group, Paper, ScrollArea , Box, Image, createStyles, Text,Checkbox} from '@mantine/core'
+import { Group, Paper, ScrollArea, Box, Image, createStyles, Text, Checkbox } from '@mantine/core'
 import React, { ChangeEvent, useEffect, useRef, useState } from 'react'
-import {  Checks } from 'tabler-icons-react'
-import { QuickAnswer ,FullMeliItem } from '../../types/types'
+import { Checks } from 'tabler-icons-react'
+import { QuickAnswer, FullMeliItem } from '../../types/types'
 import { useSetState, useDebouncedValue, upperFirst } from '@mantine/hooks'
 import { getActiveToken, getTimeAgo } from '../../helpers'
 import { useSearchMLItemQuery } from '../../hooks/useSearchMLItemsQuery'
@@ -13,68 +13,71 @@ import { useAnswerQuestion } from '../../hooks/useAnswerQuestion'
 import { useUserConfigQuery } from '../../hooks/useUserConfigQuery'
 
 const useStyles = createStyles((theme, _params, getRef) => ({
-    comboboxInput: {
-      display: 'block',
-      width: '100%',
-      height: '90px',
-      resize: 'none',
-      appearance: 'none',
-      borderRadius: '2px 0 0 2px',
-      border: '2px solid ',
-      fontSize: '0.9rem',
-      borderColor: theme.colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[3],
-      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[3],
-      padding: '10px',
-      borderRight: 0,
-      color: theme.colorScheme === 'dark' ? theme.colors.gray[0] : theme.colors.gray[8],
-      paddingRight: '56px',
-  
-      '&:focus': {
-        outline: 'none',
-        borderColor: theme.colors.blue[7],
-        borderWidth: '2px',
-        backgroundColor: theme.colorScheme === 'dark' ? theme.colors.gray[8] : theme.colors.gray[0]
-      },
-  
-      '&::placeholder': { fontStyle: 'italic' }
-    },
-    comboboxButton: {
-      position: 'absolute',
-      right: 0,
-      backgroundColor: theme.colors.blue[6],
-      border: 0,
-      height: '100%',
-      paddingInline: '10px',
-      cursor: 'pointer'
-    },
-    // className='scrollbar-hide absolute z-10 max-h-32 w-max overflow-y-auto px-2 text-sm'
-  
-    comboboxOptions: {
-      position: 'absolute',
-      zIndex: 10,
-      maxHeight: 60,
-      paddingBlock: 2
-    },
-    // className={`flex min-w-[150px] cursor-pointer items-center justify-start space-x-2 rounded-sm px-1 py-1 text-left shadow-md transition-all duration-100 ease-in-out ${
-    // 	active ? 'bg-purple-600' : 'bg-white'
-    // }`}
-    comboboxOption: {
-      display: 'flex',
-      minWidth: 150,
-      cursor: 'pointer',
-      alignItems: 'center',
-      justifyContent: 'start'
-    }
-  }))
+  comboboxInput: {
+    display: 'block',
+    width: '100%',
+    height: '90px',
+    resize: 'none',
+    appearance: 'none',
+    borderRadius: '2px 0 0 2px',
+    border: '2px solid ',
+    fontSize: '0.9rem',
+    borderColor: theme.colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[3],
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.gray[9] : theme.colors.gray[3],
+    padding: '10px',
+    borderRight: 0,
+    color: theme.colorScheme === 'dark' ? theme.colors.gray[0] : theme.colors.gray[8],
+    paddingRight: '56px',
 
+    '&:focus': {
+      outline: 'none',
+      borderColor: theme.colors.blue[7],
+      borderWidth: '2px',
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.gray[8] : theme.colors.gray[0]
+    },
 
-  interface InputWithAutocompleteProps {
-      answerQuestion: UseMutateFunction
-      questionId: number;
-      dateCreated: Date
+    '&::placeholder': { fontStyle: 'italic' }
+  },
+  comboboxButton: {
+    position: 'absolute',
+    right: 0,
+    backgroundColor: theme.colors.blue[6],
+    border: 0,
+    height: '100%',
+    paddingInline: '10px',
+    cursor: 'pointer'
+  },
+  // className='scrollbar-hide absolute z-10 max-h-32 w-max overflow-y-auto px-2 text-sm'
+
+  comboboxOptions: {
+    position: 'absolute',
+    zIndex: 10,
+    maxHeight: 60,
+    paddingBlock: 2
+  },
+  // className={`flex min-w-[150px] cursor-pointer items-center justify-start space-x-2 rounded-sm px-1 py-1 text-left shadow-md transition-all duration-100 ease-in-out ${
+  // 	active ? 'bg-purple-600' : 'bg-white'
+  // }`}
+  comboboxOption: {
+    display: 'flex',
+    minWidth: 150,
+    cursor: 'pointer',
+    alignItems: 'center',
+    justifyContent: 'start'
   }
+}))
 
-const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({ answerQuestion , questionId, dateCreated}) => {
+interface InputWithAutocompleteProps {
+  answerQuestion: UseMutateFunction
+  questionId: number
+  dateCreated: Date
+}
+
+const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({
+  answerQuestion,
+  questionId,
+  dateCreated
+}) => {
   const { classes, theme } = useStyles()
 
   const queryClient = useQueryClient()
@@ -102,7 +105,7 @@ const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({ answerQue
       }
 
       // console.log(finalAnswer);
-    answerQuestion({
+      answerQuestion({
         id: questionId,
         answer: finalAnswer
       } as unknown as void)
@@ -122,9 +125,7 @@ const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({ answerQue
     }
   }
 
-
-
-      // State and queries for Listings and Quick Answers autocomplete
+  // State and queries for Listings and Quick Answers autocomplete
 
   interface AutocompleteState {
     query: string
@@ -160,7 +161,6 @@ const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({ answerQue
       }
     }
   }
-
 
   const searchQuickAnswers = QuickAnswersQuery.refetch
 
@@ -217,199 +217,198 @@ const InputWithAutocomplete: React.FC<InputWithAutocompleteProps> = ({ answerQue
   }, [query, searchMlItems, searchQuickAnswers])
 
   // End of autocomplete
-    const drop = (e: React.DragEvent<HTMLTextAreaElement>) => {
-        const value = e.dataTransfer.getData('value')
-    
-        setAnswer(`${answer}${value.trim()} `)
-        textareaRef.current!.focus()
-      }
-    
-      const dragOver = (e: React.DragEvent<HTMLTextAreaElement>) => {
-        e.preventDefault()
-        e.stopPropagation()
-      }
+  const drop = (e: React.DragEvent<HTMLTextAreaElement>) => {
+    const value = e.dataTransfer.getData('value')
 
-  return <>
-                <Group align="end" position="apart" mb={0}>
-                  {userConfig?.data?.data.preAnswerMsg && (
-                    <Checkbox
-                      checked={hello}
-                      onChange={(e) => setHello(e.target.checked)}
-                      label={`${userConfig?.data?.data.preAnswerMsg}`}
-                      radius="xs"
-                      size="sm"
-                      mb="xs"
-                    />
-                  )}
-                  <Text
-                    size="xs"
-                    sx={{
-                      fontStyle: 'italic',
-                      marginLeft: 'auto'
-                    }}
-                  >
-                    {upperFirst(getTimeAgo(new Date(dateCreated).getTime()))}
-                  </Text>
-                </Group>
-  <Group sx={{ position: 'relative', width: '100%' }} noWrap mb={12}>
-  <Combobox
-    as="div"
-    style={{ width: '100%' }}
-    // @ts-ignore
-    onChange={(value: QuickAnswer | FullMeliItem) => {
-      if ('permalink' in value) {
-        insertValue(value.permalink)
-      } else {
-        insertValue(value.text)
-      }
-      setAutocomplete({ results: [] })
-    }}
-    value={answer}
-  >
-    <Combobox.Input
-      as="textarea"
-      className={classes.comboboxInput}
-      style={{
-        borderColor: error ? theme.colors.red[7] : ''
-      }}
-      id={`answer-textarea`}
-      placeholder="Ingresa tu respuesta"
-      maxLength={2000}
-      onChange={handleInput}
-      ref={textareaRef}
-      onKeyUp={handleKeyUp}
-      onDrop={drop}
-      onDragOver={dragOver}
-    />
-    {shouldOpen && autocomplete.results.length > 0 && (
-      <Combobox.Options
-        static
-        className={classes.comboboxOptions}
-        style={{
-          top: position.top + position.height - 15,
-          left: position.left - 30
-        }}
-      >
-        <Paper
-          component={ScrollArea}
-          shadow="md"
-          radius="xs"
-          offsetScrollbars
-          sx={(theme) => ({
-            height: 160
-          })}
-        >
-          {autocomplete.results.map((value: QuickAnswer | FullMeliItem) => (
-            <Combobox.Option
-              key={'id' in value ? value.id : value._id}
-              value={value}
-              as="div"
-            >
-              {({ active }) => (
-                <Group
-                  className={classes.comboboxOption}
-                  spacing={12}
-                  position="apart"
-                  p={4}
-                  sx={(theme) => ({
-                    borderLeft: 4,
-                    borderLeftStyle: 'solid',
-                    borderColor: active ? theme.colors.blue[9] : 'transparent'
-                  })}
-                >
-                  {'secure_thumbnail' in value && (
-                    <Box>
-                      <Image
-                        src={value.secure_thumbnail}
-                        alt={value.title}
-                        fit="contain"
-                        sx={(theme) => ({
-                          width: '1.5rem',
-                          height: '1.5rem'
-                        })}
-                      />
-                    </Box>
-                  )}
-                  <Text
-                    size="sm"
-                    transform={'name' in value ? 'uppercase' : 'none'}
-                  >
-                    {'name' in value ? value.name : value.title}
-                  </Text>
-                  {'price' in value && <Text size="sm">${value.price}</Text>}
-                </Group>
-              )}
-            </Combobox.Option>
-          ))}
-        </Paper>
-      </Combobox.Options>
-    )}
-    {/^[@#]\w{1,30}$/.test(query) && autocomplete.results.length === 0 && (
-      <Combobox.Options
-        static
-        className={classes.comboboxOptions}
-        style={{
-          top: position.top + position.height - 15,
-          left: position.left - 30
-        }}
-      >
-        {MlItemsQuery.isLoading || QuickAnswersQuery.isLoading ? (
-          <Paper p={4} radius="xs">
-            <Text size="sm">Cargando...</Text>
-          </Paper>
-        ) : (
-          <Paper p={4} radius="xs">
-            <Text size="sm">No se encontraron resultados.</Text>
-          </Paper>
+    setAnswer(`${answer}${value.trim()} `)
+    textareaRef.current!.focus()
+  }
+
+  const dragOver = (e: React.DragEvent<HTMLTextAreaElement>) => {
+    e.preventDefault()
+    e.stopPropagation()
+  }
+
+  return (
+    <>
+      <Group align="end" position="apart" mb={0}>
+        {userConfig?.data?.data.preAnswerMsg && (
+          <Checkbox
+            checked={hello}
+            onChange={(e) => setHello(e.target.checked)}
+            label={`${userConfig?.data?.data.preAnswerMsg}`}
+            radius="xs"
+            size="sm"
+            mb="xs"
+          />
         )}
-      </Combobox.Options>
-    )}
-  </Combobox>
+        <Text
+          size="xs"
+          sx={{
+            fontStyle: 'italic',
+            marginLeft: 'auto'
+          }}
+        >
+          {upperFirst(getTimeAgo(new Date(dateCreated).getTime()))}
+        </Text>
+      </Group>
+      <Group sx={{ position: 'relative', width: '100%' }} noWrap mb={12}>
+        <Combobox
+          as="div"
+          style={{ width: '100%' }}
+          // @ts-ignore
+          onChange={(value: QuickAnswer | FullMeliItem) => {
+            if ('permalink' in value) {
+              insertValue(value.permalink)
+            } else {
+              insertValue(value.text)
+            }
+            setAutocomplete({ results: [] })
+          }}
+          value={answer}
+        >
+          <Combobox.Input
+            as="textarea"
+            className={classes.comboboxInput}
+            style={{
+              borderColor: error ? theme.colors.red[7] : ''
+            }}
+            id={`answer-textarea`}
+            placeholder="Ingresa tu respuesta"
+            maxLength={2000}
+            onChange={handleInput}
+            ref={textareaRef}
+            onKeyUp={handleKeyUp}
+            onDrop={drop}
+            onDragOver={dragOver}
+          />
+          {shouldOpen && autocomplete.results.length > 0 && (
+            <Combobox.Options
+              static
+              className={classes.comboboxOptions}
+              style={{
+                top: position.top + position.height - 15,
+                left: position.left - 30
+              }}
+            >
+              <Paper
+                component={ScrollArea}
+                shadow="md"
+                radius="xs"
+                offsetScrollbars
+                sx={(theme) => ({
+                  height: 160
+                })}
+              >
+                {autocomplete.results.map((value: QuickAnswer | FullMeliItem) => (
+                  <Combobox.Option
+                    key={'id' in value ? value.id : value._id}
+                    value={value}
+                    as="div"
+                  >
+                    {({ active }) => (
+                      <Group
+                        className={classes.comboboxOption}
+                        spacing={12}
+                        position="apart"
+                        p={4}
+                        sx={(theme) => ({
+                          borderLeft: 4,
+                          borderLeftStyle: 'solid',
+                          borderColor: active ? theme.colors.blue[9] : 'transparent'
+                        })}
+                      >
+                        {'secure_thumbnail' in value && (
+                          <Box>
+                            <Image
+                              src={value.secure_thumbnail}
+                              alt={value.title}
+                              fit="contain"
+                              sx={(theme) => ({
+                                width: '1.5rem',
+                                height: '1.5rem'
+                              })}
+                            />
+                          </Box>
+                        )}
+                        <Text size="sm" transform={'name' in value ? 'uppercase' : 'none'}>
+                          {'name' in value ? value.name : value.title}
+                        </Text>
+                        {'price' in value && <Text size="sm">${value.price}</Text>}
+                      </Group>
+                    )}
+                  </Combobox.Option>
+                ))}
+              </Paper>
+            </Combobox.Options>
+          )}
+          {/^[@#]\w{1,30}$/.test(query) && autocomplete.results.length === 0 && (
+            <Combobox.Options
+              static
+              className={classes.comboboxOptions}
+              style={{
+                top: position.top + position.height - 15,
+                left: position.left - 30
+              }}
+            >
+              {MlItemsQuery.isLoading || QuickAnswersQuery.isLoading ? (
+                <Paper p={4} radius="xs">
+                  <Text size="sm">Cargando...</Text>
+                </Paper>
+              ) : (
+                <Paper p={4} radius="xs">
+                  <Text size="sm">No se encontraron resultados.</Text>
+                </Paper>
+              )}
+            </Combobox.Options>
+          )}
+        </Combobox>
 
-  <button
-    className={classes.comboboxButton}
-    onClick={handleAnswer}
-    style={{
-      backgroundColor: error ? theme.colors.red[7] : ''
-    }}
-  >
-    <Checks color="white" size={32} />
-  </button>
-</Group>
+        <button
+          className={classes.comboboxButton}
+          onClick={handleAnswer}
+          style={{
+            backgroundColor: error ? theme.colors.red[7] : ''
+          }}
+        >
+          <Checks color="white" size={32} />
+        </button>
+      </Group>
 
-{userConfig?.data?.data.postAnswerMsg ? (
-    <Checkbox
-      checked={goodbye}
-      onChange={(e) => setGoodbye(e.target.checked)}
-      radius="xs"
-      size="sm"
-      label={`${userConfig?.data?.data.postAnswerMsg}`}
-    />
-  ) : (
-    <Text size="xs" sx={{ fontStyle: 'italic' }}>
-      Podes configurar tu saludo y despedida desde opciones.
-    </Text>
-  )}
-
-  {error ? (
-    <Text size="xs" sx={{ fontStyle: 'italic' }} color="red" mt="md">
-      {error}
-    </Text>
-  ) : (
-    <Text
-      sx={(theme) => ({
-        fontStyle: 'italic',
-        color: theme.colors.gray[6]
-      })}
-      size="xs"
-      mt="md"
-    >
-      Tipeá &apos;@&apos; para respuestas rápidas o &apos;#&apos; para insertar un
-      link a otra publicación. Usá Ctrl + Enter para enviar la respuesta.
-    </Text>
-  )}
-  </>
-  
+      {error ? (
+        <Text size="xs" sx={{ fontStyle: 'italic' }} color="red" mt={0} mb="md">
+          {error}
+        </Text>
+      ) : (
+        <Text
+          sx={(theme) => ({
+            fontStyle: 'italic',
+            color: theme.colors.gray[6]
+          })}
+          size="xs"
+          mt={0}
+          mb="md"
+        >
+          Tipeá &apos;@&apos; para respuestas rápidas o &apos;#&apos; para insertar un link a otra
+          publicación. Usá Ctrl + Enter para enviar la respuesta.
+        </Text>
+      )}
+      {userConfig?.data?.data.postAnswerMsg ? (
+        <Checkbox
+          checked={goodbye}
+          onChange={(e) => setGoodbye(e.target.checked)}
+          radius="xs"
+          size="sm"
+          my="md"
+          label={`${userConfig?.data?.data.postAnswerMsg}`}
+        />
+      ) : (
+        <Text size="xs" sx={{ fontStyle: 'italic' }}>
+          Podes configurar tu saludo y despedida desde opciones.
+        </Text>
+      )}
+    </>
+  )
 }
 
 export default InputWithAutocomplete
